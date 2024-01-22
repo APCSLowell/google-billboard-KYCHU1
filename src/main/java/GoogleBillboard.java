5c4cd2d6a4176c6fcffa2c9747e72cf0143477ef
KYCHU1 <142847721+KYCHU1@users.noreply.github.com>
2024-01-22 12:49:07 -0800

    Update GoogleBillboard.java

--- src/main/java/GoogleBillboard.java
+++ src/main/java/GoogleBillboard.java
@@ -16,9 +16,9 @@ public class GoogleBillboard {
 
 	//Finish this function
 	public boolean isPrime(double dNum){
-	if (dNum < 2){
-    		return false;
-  	}
+		if (dNum < 2){
+    			return false;
+  		}
   	for (int i = 2; i <= Math.sqrt(dNum); i++){
     		if (dNum % i == 0){
       		return false;
