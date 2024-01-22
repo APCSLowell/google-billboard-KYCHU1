2919a4fe27ca5c7125d723766f4616c36ee42a84
KYCHU1 <142847721+KYCHU1@users.noreply.github.com>
2024-01-22 12:47:06 -0800

    Update GoogleBillboard.java

--- src/main/java/GoogleBillboard.java
+++ src/main/java/GoogleBillboard.java
@@ -3,13 +3,28 @@
 public class GoogleBillboard {
 	public final static String e = "2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435";  
 	public static void main(final String[] args) {
-		System.out.println("Replace this string with your answer!");
+		for(int i = 9; i < e.length()-9; i++){
+    			String digits = e.substring(i,i+10);
+    			dNum = Double.parseDouble(digits);
+    			if (isPrime(dNum)){
+      				System.out.println(dNum);
+      				break;
+    			}
+  		}
 	}
 	
 
 	//Finish this function
-	public boolean isPrime(double d){
+	public boolean isPrime(double dNum){
+	if (dNum < 2){
+    		return false;
+  	}
+  	for (int i = 2; i <= Math.sqrt(dNum); i++){
+    		if (dNum % i == 0){
+      		return false;
+    		}
+  	}
+  	return true; 
 	}
-
 }
 
